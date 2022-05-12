f627239df4435f40bf68262e36e631f9226dfe28
MONKEY-BASE\monke <monkey@w-field.info>
2022-05-13 00:39:45 +0900

    List2-9 HelloEventプロジェクトの実装

--- app/src/main/java/com/example/learning_project/MainActivity.kt
+++ app/src/main/java/com/example/learning_project/MainActivity.kt
@@ -3,9 +3,24 @@ package com.example.learning_project
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//List2-9で追加
+import android.view.View
+import android.widget.TextView
+
+import java.util.Date
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+        //List2-9で追加
+        fun btnCurrentClick(v:View){
+            val txtResult = findViewById<TextView>(R.id.txtReslut)
+            txtResult.text = Date().toString()
+
+        }
+
+
 }
